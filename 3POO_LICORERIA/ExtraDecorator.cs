5741dc1752332ea90406d1bfac3c3ac9aab19406
PMtradefx <e1313836304@live.uleam.edu.ec>
2023-12-17 12:10:37 -0500

    Agregar archivos de proyecto.

---
+++ 3POO_LICORERIA/ExtraDecorator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.text;
+
+namespace _3POO_Licoreria
+{
+    public abstract class ExtraDecorator : DatosUsuario
+    {
+        protected DatosUsuario Extras;
+        public ExtraDecorator(DatosUsuario Extra)
+        {
+            Extras = Extra;
+        }
+    }
+}
+     
